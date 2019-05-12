b2a90735a326a33e966426893530e6c48efdded1
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-12 19:40:02 +0300

    Add django admin

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Image,Location,categories
 
 # Create your tests here.
+class ImageTestclass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.machine = categories.objects.create(name='machine')
+        self.beast = categories.objects.create(name='beast')
+        self.mombasa = Location.objects.create(name='Mombasa')
+
+        self.car = Image.objects.create(
+            name='skyline', location='self.mombasa', description='Quite a fast car')
+
+        self.car.categories.add(self.machine)
+        self.car.categories.add(self.beast)   
+
+        
+    # Testing  instance
+    def test_instance(self):
+        self.car.save()
+        self.assertTrue(isinstance(self.car,Image))    
