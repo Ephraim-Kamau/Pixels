6376eef9bcbf8ba09ee0c4e106c875c355c7285e
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-12 17:25:29 +0300

    Add the templates folder with the various html pages

--- images/views.py
+++ images/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse,Http404
+from django.shortcuts import render,redirect,Http404
 import datetime as dt
 
 # Create your views here.
@@ -21,22 +21,19 @@ def convert_dates(dates):
     day = days[day_number]
     return day    
 
+ # View Function to present images from past days
 def past_days_images(request,past_date):
-
+   
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
-        raise Http404()    
+        raise Http404() 
+        assert False   
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Images for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)    
+    if date == dt.date.today():
+        return redirect(images_of_day)
+  
+    return render (request, 'all-images/past-images.html', {"date": date})
