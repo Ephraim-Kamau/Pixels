1e272ebea48a54a139c5bc338518cd31448da0b9
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-12 16:28:02 +0300

    Add the functional welcome view function

--- images/views.py
+++ images/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to my Gallery')
