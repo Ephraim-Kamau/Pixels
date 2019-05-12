5a29302404f6afa9b1f359c881a84228b6bcdc6d
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-12 22:04:21 +0300

    Add changes to folder structure

--- images/views.py
+++ images/views.py
@@ -1,45 +1,14 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
-import datetime as dt
 from .models import Image
 
 # Create your views here.
-def welcome(request):
-    return render (request, 'welcome.html')
 
 def images_of_day(request):
-    date = dt.date.today()
     images = Image.objects.all()
 
-    return render(request, 'all-images/today-images.html', {"date": date, "images":images})
+    return render(request, 'today-images.html', {"images":images})
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day    
-
- # View Function to present images from past days
-def past_days_images(request,past_date):
-   
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404() 
-        assert False   
-
-    if date == dt.date.today():
-        return redirect(images_of_day)
-  
-    return render (request, 'all-images/past-images.html', {"date": date})
 
 def search_results(request):
 
@@ -48,11 +17,11 @@ def search_results(request):
         searched_images = Image.search_by_category(search_images)
         message = f"{search_images}"
 
-        return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
+        return render(request, 'search.html',{"message":message,"images": searched_images})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-images/search.html',{"message":message})    
+        return render(request, 'search.html',{"message":message})    
 
 def get_image_by_id(request,image_id):
     try:
