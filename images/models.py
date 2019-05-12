a77b3619d1ed66f697386e02550ae53226729d59
Ephraim-Kamau <kephraim19@gmail.com>
2019-05-12 19:45:40 +0300

    Add the models to admin page

---
+++ images/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class categories(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    categories = models.ManyToManyField(categories)
+    location = models.ForeignKey(Location)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()    
+
+    class Meta:
+        ordering = ['name']
